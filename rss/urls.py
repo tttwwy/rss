309f8d34c1ce36d1ec167d941dae7fb22d5e89de
xiaose <tttwwy@126.com>
2014-10-21 10:57:57 +0800

    full text

--- rss/urls.py
+++ rss/urls.py
@@ -7,6 +7,7 @@ from app.rss import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^weixin$',views.index),
-    url(r'^rss/(.+)/$', views.feed)
+    url(r'^weixin$', views.index),
+    url(r'^rss/(.+)/$', views.feed),
+    url(r'^rss_full/(.+)/$', views.full_text_feed)
 )
